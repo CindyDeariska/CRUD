6eefe3c5490552a2681b7d3265a25bc21d152c16
Cindy Dea <83697014+CindyDeariska@users.noreply.github.com>
2023-05-29 22:32:24 +0700

    views/template

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io"></a>.</strong> All rights reserved.
+</footer>
+
+<aside class="control-sidebar control-sidebar-dark">
+
+</aside>
+
+</div>
+
+
+<script src="<?php echo base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
+
+<script src="<?php echo base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="<?php echo base_url() ?>assets/dist/js/adminlte.min.js?v=3.2.0"></script>
+
+<script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script>
+</body>
+
+</html>
